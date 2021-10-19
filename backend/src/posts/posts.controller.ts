d84188291cce6f30407f8f423d60c22014c5942e
Robert Nguyen <robert@gotitapp.co>
2021-10-20 00:02:53 +0700

    feat: setup post module with CURD supports

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param, Patch, Delete } from "@nestjs/common";
 import { PostsService } from "./posts.service";
-
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
@@ -16,5 +15,35 @@ export class PostsController {
     return {
       id: generatedId
     }
+  }
+
+  @Get()
+  getPosts() {
+    const posts = this.postsService.getPosts()
+    return posts
+  }
+
+  @Get(':id')
+  getPost(@Param('id') postId: string) {
+    return this.postsService.getPostById(postId)
+  }
+
+  @Patch(':id')
+  updatePost(
+    @Param('id') postId: string,
+    @Body() completeBody: {
+     title: string,
+     description: string, 
+    }) {
+    const {title, description} = completeBody
+    return this.postsService.updatePost(postId, title, description)
+  }
+
+  @Delete(':id')
+  removePost(
+    @Param('id') postId: string,
+  ) {
+    this.postsService.removePost(postId)
+    return null
   }
 }
--- backend/src/posts/posts.service.ts
+++ backend/src/posts/posts.service.ts
@@ -1,10 +1,19 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { v4 as uuidv4 } from 'uuid';
 import { Post } from './post.model'
 
-@Injectable
+@Injectable()
 export class PostsService {
-  posts: Post[] = [];
+  private posts: Post[] = [];
+
+  private findPost(id: string): [Post, number] {
+    const index = this.posts.findIndex(item => item.id === id)
+    if (index === -1) {
+      throw new NotFoundException('Could not find post.')
+    }
+    const post = this.posts[index]
+    return [post, index]
+  }
 
   createPost(title: string, description: string) {
     const currentDateTime =  new Date().toString()
@@ -19,5 +28,36 @@ export class PostsService {
     )
     this.posts.push(newPost)
     return postId
+  }
+
+  getPosts() {
+    return this.posts.map(item => item)
+  }
+
+  getPostById(selectedId: string) {
+    const selectedPost = this.findPost(selectedId)[0]
+    if (!selectedPost) {
+      throw new NotFoundException('Could not find post.')
+    }
+    return { ...selectedPost }
+  }
+
+  updatePost(selectedId: string, title: string, description: string) {
+    const [post, index] = this.findPost(selectedId)
+    const updatedPost = {...post}
+    if (title) {
+      updatedPost.title = title
+    }
+    if (description) {
+      updatedPost.description = description
+    }
+    updatedPost.updated_at = new Date().toString()
+    this.posts[index] = updatedPost
+    return updatedPost
+  }
+
+  removePost(selectedId: string) {
+    const index = this.findPost(selectedId)[1]
+    this.posts.splice(1)
   }
 }
