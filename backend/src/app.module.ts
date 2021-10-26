f8b063984186b910dff2c1233f76ef2fa980c10b
Robert Nguyen <robert@gotitapp.co>
2021-10-26 13:49:00 +0700

    fix: clean up credential from codebase

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,13 +6,9 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 // TODO: use environment variable to save the access information to the database.
+
 @Module({
-  imports: [
-    PostsModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://robert:qz8nmKLAwdVHAyCl@biography-db.uroio.mongodb.net/bio-page?retryWrites=true&w=majority',
-    ),
-  ],
+  imports: [PostsModule, MongooseModule.forRoot(process.env.MONGO_DB_ACCESS)],
   controllers: [AppController],
   providers: [AppService],
 })
