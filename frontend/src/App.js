285cc78e8b286237b075afa789a7ddad932b6d84
Robert Nguyen <robert@gotitapp.co>
2021-11-13 22:24:05 +0700

    fix: setup basict routing for the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom'
+import Landing from './components/Landing'
+import Posts from './components/Posts'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route
+          exact
+          path="/profile"
+          component={Landing} />
+          <Route
+          exact
+          path="/posts"
+          component={Posts} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
